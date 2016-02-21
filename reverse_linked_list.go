ec2d584bf5daa680df332d44c88a2bef224a61ed
Jerry Wang <jerry.wang@delphix.com>
2016-02-20 22:39:34 -0800

    rename reverse_linkedlist.go

---
+++ reverse_linked_list.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/j3rrywan9/go-algorithms/linkedlist"
+)
+
+// LeetCode OJ No. 206
+func reverseLinkedList(head *linkedlist.Node) *linkedlist.Node {
+	var dummy *linkedlist.Node
+	for head != nil {
+		current := head
+		head = head.Next
+		current.Next = dummy
+		dummy = current
+	}
+	return dummy
+}
+
+func main() {
+	myList := &linkedlist.Node{1, nil}
+	fmt.Println("Creating a 10-node linked list...")
+	myList.CreateLinkedList(10)
+	myList.PrintLinkedList()
+
+	fmt.Println("Reversing above linked list...")
+	reversedList := reverseLinkedList(myNode)
+	reversedList.PrintLinkedList()
+}
