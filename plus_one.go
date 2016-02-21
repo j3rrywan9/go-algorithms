d9401498a0d3b2e6f35105f6e556acb868b81e43
Jerry Wang <jerry.wang@delphix.com>
2016-02-20 16:57:50 -0800

    update plus_one.go

--- plus_one.go
+++ plus_one.go
@@ -5,28 +5,42 @@ import (
 	"strconv"
 )
 
-// LCOJ No. 66
-func plusOne(digits []int) []int {
+// LeetCode OJ No. 66
+func plusOne(digits []int) int {
 	var num int
+
+	// Get the number represented by the digits
 	for i := 0; i < len(digits); i++ {
 		num = num * 10 + digits[i]	
 	}
+
+	// Convert (number + 1) to string
 	num_str := strconv.Itoa(num + 1)
-	var res []int
+
+	var digits_plus_one []int
+
 	for _, char := range(num_str) {
+		// Convert each character in the string to integer
 		i, err := strconv.Atoi(string(char))
 		if err != nil {
 			panic(err)
 		}
-		res = append(res, i)
+		digits_plus_one = append(digits_plus_one, i)
 	}
-	return res
+
+	var num_plus_one int
+
+	for i := 0; i < len(digits_plus_one); i++ {
+		num_plus_one = num_plus_one * 10 + digits_plus_one[i]	
+	}
+
+	return num_plus_one 
 }
 
 func main() {
-	mydigits := []int{3, 8, 2, 5}
-	fmt.Printf("Input: %v\n", mydigits)
-	mydigitsplusone := plusOne(mydigits)
-	fmt.Printf("Output: %v\n", mydigitsplusone)
+	digits := []int{3, 8, 2, 5}
+	fmt.Printf("Input: %v\n", digits)
+	digits_plus_one := plusOne(digits)
+	fmt.Printf("Output: %d\n", digits_plus_one)
 }
 
