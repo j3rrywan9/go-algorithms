5244acabe3e10b2b708bcab243adc3ff91508970
Jerry Wang <jerry.wang@delphix.com>
2016-02-20 17:06:45 -0800

    update plus_one.go

--- plus_one.go
+++ plus_one.go
@@ -6,7 +6,7 @@ import (
 )
 
 // LeetCode OJ No. 66
-func plusOne(digits []int) int {
+func plusOne(digits []int) []int {
 	var num int
 
 	// Get the number represented by the digits
@@ -28,19 +28,13 @@ func plusOne(digits []int) int {
 		digits_plus_one = append(digits_plus_one, i)
 	}
 
-	var num_plus_one int
-
-	for i := 0; i < len(digits_plus_one); i++ {
-		num_plus_one = num_plus_one * 10 + digits_plus_one[i]	
-	}
-
-	return num_plus_one 
+	return digits_plus_one
 }
 
 func main() {
 	digits := []int{3, 8, 2, 5}
 	fmt.Printf("Input: %v\n", digits)
 	digits_plus_one := plusOne(digits)
-	fmt.Printf("Output: %d\n", digits_plus_one)
+	fmt.Printf("Output: %v\n", digits_plus_one)
 }
 
