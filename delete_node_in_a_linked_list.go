5d0ed5e1bcec944ff11832931f9592c0c6a5c629
Jerry Wang <jerry.wang@delphix.com>
2016-02-20 22:53:15 -0800

    add delete_node_in_a_linked_list.go

---
+++ delete_node_in_a_linked_list.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/j3rrywan9/go-algorithms/linkedlist"
+)
+
+// LeetCode OJ No. 237
+func deleteNode(node *linkedlist.Node) {
+	*node = *node.Next
+}
+
+func main() {
+	myList := &linkedlist.Node{1, nil}
+	fmt.Println("Creating a 10-node linked list...")
+	myList.Create(10)
+	myList.Print()
+
+	fmt.Println("Deleting 3rd node from above linked list...")
+	var myNode *linkedlist.Node = myList
+	for i := 0; i < 2; i++ {
+		myNode = myNode.Next
+	}
+	deleteNode(myNode)
+	myList.Print()
+}
