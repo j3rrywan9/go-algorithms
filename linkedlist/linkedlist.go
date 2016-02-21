7ae5619a75d3f6eb646bef99d6962b45efc488ea
Jerry Wang <jerry.wang@delphix.com>
2016-02-20 22:26:48 -0800

    add linkedlist/linkedlist.go

---
+++ linkedlist/linkedlist.go
@@ -0,0 +1,38 @@
+package linkedlist
+
+import (
+	"fmt"
+)
+
+// Node of a linked list
+type Node struct {
+	Val int
+	Next *Node
+}
+
+// Create a linked list of specified length
+func (this *Node) Create(length int) {
+	current := this
+	for i := 2; i <= length; i++ {
+		pNode := &Node{i, nil}
+ 		current.Next = pNode
+		current = pNode
+    }
+}
+
+// Print the value of each node of a linked list
+func (this *Node) Print() {
+	current := this
+	for {
+		fmt.Printf("%d", current.Val)
+
+		if current.Next != nil {
+			fmt.Print("->")
+			current = current.Next
+		} else {
+			break
+		}
+	}
+	fmt.Println()
+}
+
