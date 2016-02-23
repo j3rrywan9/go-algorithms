044783f9fce57eae2e5ea54c7079247a47924f7a
Jerry Wang <jerry.wang@delphix.com>
2016-02-23 14:45:45 -0800

    add remove_nth_node_from_end_of_list.go

---
+++ remove_nth_node_from_end_of_list.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/j3rrywan9/go-algorithms/linkedlist"
+)
+
+// LeetCode OJ No. 19
+func removeNthNodeFromEndOfList(head *linkedlist.Node, n int) *linkedlist.Node {
+	fast, slow := head, head
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+	// If n th node from end is the same as head, return head.Next directly
+	if fast == nil {
+		return head.Next
+	}
+	for fast.Next != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return head
+}
+
+func main() {
+	myList := &linkedlist.Node{1, nil}
+	fmt.Println("Creating a 10-node linked list...")
+	myList.Create(10)
+	myList.Print()
+
+	fmt.Println("Removing 4th node from end of above linked list...")
+	myList = removeNthNodeFromEndOfList(myList, 4)
+	myList.Print()
+}
