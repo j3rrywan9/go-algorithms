f3bd3309c8d089ba6cc86f8d98b41710754977d3
Jerry Wang <j3rry.wan9@gmail.com>
2019-06-17 10:44:45 -0700

    run go fmt

--- insertion_sort.go
+++ insertion_sort.go
@@ -9,8 +9,8 @@ func insertionSort(array []int) {
 	for i := 1; i < len(array); i++ {
 		value = array[i]
 		hole = i
-		for hole > 0 && array[hole - 1] > value {
-			array[hole] = array[hole - 1]
+		for hole > 0 && array[hole-1] > value {
+			array[hole] = array[hole-1]
 			hole -= 1
 		}
 		array[hole] = value
@@ -23,4 +23,3 @@ func main() {
 	insertionSort(a)
 	fmt.Println("Sorted array: ", a)
 }
-
