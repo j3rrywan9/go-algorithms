a5da61ba08fcd0220069a7008738f7051afdf0a8
Jerry Wang <j3rry.wan9@gmail.com>
2019-06-18 11:14:45 -0700

    rename index_of_pivot to partitionIndex

--- quick_sort.go
+++ quick_sort.go
@@ -9,35 +9,35 @@ import (
 // than the pivot are towards the right of it, then returns the index of the
 // pivot after rearrangement
 func partition(array []int, start, end int) int {
-	index_of_pivot, pivot := start, array[end]
+	partitionIndex, pivot := start, array[end]
 
 	for i := start; i < end; i++ {
 		if array[i] <= pivot {
 			temp := array[i]
-			array[i] = array[index_of_pivot]
-			array[index_of_pivot] = temp
-			index_of_pivot += 1
+			array[i] = array[partitionIndex]
+			array[partitionIndex] = temp
+			partitionIndex += 1
 		}
 	}
 
-	// Swap the element at index of pivot with pivot
-	temp := array[index_of_pivot]
-	array[index_of_pivot] = array[end]
+	// Swap the element at the partition index with the element used as pivot
+	temp := array[partitionIndex]
+	array[partitionIndex] = array[end]
 	array[end] = temp
 
-	return index_of_pivot
+	return partitionIndex
 }
 
 func quickSort(array []int, start, end int) {
 	if start < end {
 		// Partition the unsorted array
-		index_of_pivot := partition(array, start, end)
+		partitionIndex := partition(array, start, end)
 
-		// Quick sort the segment left to the pivot index
-		quickSort(array, start, index_of_pivot-1)
+		// Quick sort the segment left to the partition index
+		quickSort(array, start, partitionIndex-1)
 
-		// Quick sort the segment right to the pivot index
-		quickSort(array, index_of_pivot+1, end)
+		// Quick sort the segment right to the partition index
+		quickSort(array, partitionIndex+1, end)
 	}
 }
 
