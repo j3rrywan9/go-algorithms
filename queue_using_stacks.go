0199b0fd3086e97ed8bd82d967f8aafadcd1022f
Jerry Wang <j3rry.wan9@gmail.com>
2019-06-19 00:38:24 -0700

    run go fmt

--- queue_using_stacks.go
+++ queue_using_stacks.go
@@ -7,7 +7,7 @@ import (
 type Queue struct {
 	stack1 []int
 	stack2 []int
-	size int
+	size   int
 }
 
 func (this *Queue) Push(x int) {
@@ -17,14 +17,14 @@ func (this *Queue) Push(x int) {
 
 func (this *Queue) Pop() {
 	if this.stack2 != nil {
-		this.stack2 = this.stack2[:len(this.stack2) - 1]
+		this.stack2 = this.stack2[:len(this.stack2)-1]
 	} else {
-		for i := 0; i < this.size - 1; i++ {
-			x := this.stack1[len(this.stack1) - 1]
-			this.stack1 = this.stack1[:len(this.stack1) - 1]
+		for i := 0; i < this.size-1; i++ {
+			x := this.stack1[len(this.stack1)-1]
+			this.stack1 = this.stack1[:len(this.stack1)-1]
 			this.stack2 = append(this.stack2, x)
 		}
-		this.stack1 = this.stack1[:len(this.stack1) - 1]
+		this.stack1 = this.stack1[:len(this.stack1)-1]
 	}
 	this.size -= 1
 }
@@ -34,8 +34,8 @@ func (this *Queue) Peek() int {
 		return this.stack2[len(this.stack2)-1]
 	} else {
 		for i := 0; i < this.size; i++ {
-			x := this.stack1[len(this.stack1) - 1]
-			this.stack1 = this.stack1[:len(this.stack1) - 1]
+			x := this.stack1[len(this.stack1)-1]
+			this.stack1 = this.stack1[:len(this.stack1)-1]
 			this.stack2 = append(this.stack2, x)
 		}
 		return this.stack2[len(this.stack2)-1]
@@ -66,4 +66,3 @@ func main() {
 	myqueue.Print()
 	fmt.Printf("Is queue empty? %t\n", myqueue.Empty())
 }
-
