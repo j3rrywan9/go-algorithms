994ba7fcfb935e3e897c07e9c7ac5f8754554385
Jerry Wang <j3rry.wan9@gmail.com>
2019-07-02 13:36:37 -0700

    initialize struct with field names specified

--- remove_linked_list_elements.go
+++ remove_linked_list_elements.go
@@ -8,7 +8,7 @@ import (
 // LeetCode OJ No. 203
 func removeElements(head *linkedlist.Node, val int) *linkedlist.Node {
 	// Create a dummy node and link it before head
-	dummyNode := &linkedlist.Node{-1, nil}
+	dummyNode := &linkedlist.Node{Val: -1, Next: nil}
 	dummyNode.Next = head
 	current := dummyNode
 
@@ -24,7 +24,7 @@ func removeElements(head *linkedlist.Node, val int) *linkedlist.Node {
 }
 
 func main() {
-	myList := &linkedlist.Node{1, nil}
+	myList := &linkedlist.Node{Val: 1, Next: nil}
 	fmt.Println("Creating a 10-node linked list...")
 	myList.Create(10)
 	myList.Print()
