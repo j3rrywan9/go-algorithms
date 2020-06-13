ebd7160570502edd4784cc6db94b9c6dbc884886
Jerry Wang <j3rry.wan9@gmail.com>
2020-06-12 23:47:31 -0700

    declare an empty slice instead of use make

--- two_sum.go
+++ two_sum.go
@@ -16,7 +16,7 @@ func integerInSlice(i int, s []int) bool {
 
 // Construct a slice of integers that come from keys of a map[int]int
 func sliceOfKeys(m map[int]int) []int {
-	keys := make([]int, len(m))
+	var keys []int
 	for k := range m {
 		keys = append(keys, k)
 	}
