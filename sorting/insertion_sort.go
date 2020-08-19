efee912bbc1d84cb899f926a2aa2a11b3ba1be27
Jerry Wang <j3rry.wan9@gmail.com>
2020-08-19 16:39:25 -0700

    move Insertion Sort

--- sorting/insertion_sort.go
+++ sorting/insertion_sort.go
@@ -1,25 +1,17 @@
-package main
-
-import (
-	"fmt"
-)
+package sorting
 
 func insertionSort(array []int) {
 	var value, hole int
+
 	for i := 1; i < len(array); i++ {
 		value = array[i]
 		hole = i
+
 		for hole > 0 && array[hole-1] > value {
 			array[hole] = array[hole-1]
 			hole -= 1
 		}
+
 		array[hole] = value
 	}
-}
-
-func main() {
-	a := []int{1, 6, 2, 4, 9, 0, 5, 3, 7, 8}
-	fmt.Println("Unsorted array: ", a)
-	insertionSort(a)
-	fmt.Println("Sorted array: ", a)
 }
